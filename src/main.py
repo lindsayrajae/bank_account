a202327c034a4899fa6c78a85e7c34307b0f1e82
lindsayrajae <rajaelindsay09@gmail.com>
2020-02-25 19:39:08 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
+from models import Bank_Account
+from models import Transaction
 #from models import Person
 
 app = Flask(__name__)
@@ -36,6 +38,72 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/check_balance',methods=['POST'])
+def account_vie():
+    data = request.get_json()
+    bank = Bank_Account(
+        fullname = data["fullname"],
+        account_num = data["account_num"],
+        account_amount =data["account_amount"],
+        password = data['password'],
+        # account_email  data['account_email']
+    )
+    db.session.add(bank)
+    db.session.commit()
+    return jsonify(bank.serialize())
+
+@app.route('/login',methods=['POST'])
+def login():
+    json = request.get_json()
+    user = Bank_Account.query.filter_by(
+        fullname=json['fullname'], password=json['password']).first()
+    if user is None:
+        return 'user not found' , 404
+
+    return jsonify(user.serialize())
+
+
+@app.route('/transactions',methods=['POST'])
+def trans():
+    data = request.get_json()
+
+    db.session.add( Transaction(
+        bank_transaction_id = data[ 'bank_transaction_id'],
+        date=data['date'],
+        state=data['state'],
+        time=data['time'],
+        city=data['city'],
+        amount=data['amount'],
+
+        # account_email  data['account_email']
+    ))
+    db.session.commit()
+    return 'Transaction added successfully'
+
+
+@app.route('/BankAccount',methods=['PUT'])
+def bnk():
+    json = request.get_json()
+    user = Bank_Account.query.filter_by(
+         fullname = 'rajaw lomwdnd ',
+        # account_amount='$250', 
+        password ='iamabigwag ').first()
+    if user is None:
+        return 'user not found' , 404
+    
+    user.account_num=json['new_account_num']
+    db.session.commit()
+    return jsonify(user.serialize())
+
+
+@app.route('/bank_account')
+def get_bank():
+
+    return jsonify( Bank_Account.query.get(1).serialize() )
+
+    
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,67 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Person(db.Model):
+    __tablename__='person'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<Person %r>' % self.username
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "username": self.username,
+            "email": self.email
+        }
+
+
+class Bank_Account(db.Model):
+    __tablename__='bank_account'
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(180), unique=True, nullable=False)
+    password = db.Column(db.String(200),unique = True,nullable=False)
+    account_num = db.Column(db.String(120), unique=True, nullable=False)
+    account_amount = db.Column(db.String(120), unique=True, nullable=False)
+    # account_email = db.Column(db.String(120), unique=True, nullable=False)
+    transaction = db.relationship('Transaction',back_populates='bank_account')
+
+    def __repr__(self):
+        return '<Bank_Account %r>' % self.fullname
+
+        
+    def serialize(self):
+        return {
+            "id":self.id,
+            "fullname": self.fullname,
+            "password": self.password,
+            "account_amount":self.account_amount,
+            "fullname": self.fullname,
+            'transaction':[x.serialize()for x in self.transaction]
+            # "account_email": self.account_email,
+
+    }
+class Transaction(db.Model):
+    __tablename__='transaction'
+    id = db.Column(db.Integer,primary_key=True)
+    bank_transaction_id=db.Column(db.Integer,db.ForeignKey('bank_account.id'))
+    date= db.Column(db.String(180))
+    time= db.Column(db.String(180))
+    amount= db.Column(db.String(180))
+    item= db.Column(db.String(180))
+    state=db.Column(db.String(180))
+    city =db.Column(db.String(180))
+    bank_account = db.relationship('Bank_Account',back_populates='transaction')
+
+    def serialize(self):
+        return {
+        'date':self.date,
+        'time':self.time,
+        'amount':self.amount,
+        'item':self.item,
+        'state':self.state,
+        'city':self.city
+        
+        }
+
